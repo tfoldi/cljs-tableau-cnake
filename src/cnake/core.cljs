fa3e1324f9a037de586ad027c771565c11eead36
Gyula Laszlo <gyula.laszlo.gm@gmail.com>
2015-08-26 16:45:47 +0200

    Place the keyboard focus on the game after the tableau visualization loads

--- src/cnake/core.cljs
+++ src/cnake/core.cljs
@@ -10,4 +10,13 @@
 ;; the game. But only after Tableau viz is in interactive mode
 (go
   (async/<! tableau/tableau-viz-ready-channel)
+  
+  ;; After the tableau visualization has started, we need to re-focus
+  ;; on the game div to get the keyboard inputs correctly.
+  ;;
+  ;; For some reason, calling focus() on document.body does not do
+  ;; the job, so we simply blur the iframe that is already focused by
+  ;; Tableau.
+  (-> js/document (.getElementsByTagName "iframe") (aget 0) .blur)
+  
   (ui/init))
